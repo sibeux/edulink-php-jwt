0fccf4377f661fcbd5a4e0fe0d7073437a37c7f6
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-05-26 14:28:39 +0700

    refactor: update email sender address in OTP notification

--- api/otp.php
+++ api/otp.php
@@ -54,7 +54,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $mail->Port = 587;
 
         // Recipients
-        $mail->setFrom('wahabinasrul@gmail.com', 'Edulink');
+        $mail->setFrom('admin@edulink.sibeux.my.id', 'Edulink');
         $mail->addAddress($email);
 
         // Content
