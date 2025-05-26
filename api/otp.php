3efa1b36fd638b749cd70b628ffdfb19a1d22ff4
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-05-26 16:02:35 +0700

    false default

--- api/otp.php
+++ api/otp.php
@@ -15,13 +15,13 @@ function generateOTP($length = 4)
 }
 
 function sendOtpToDatabase($email, $otp, $db){
-    if ($stmt = $db->prepare('INSERT INTO otp (id, email, code, created_at, expires_at, is_used)
-    VALUES (NULL, ?, ?, NOW(), NOW() + INTERVAL 10 MINUTE, false)
+    if ($stmt = $db->prepare("INSERT INTO otp (id, email, code, created_at, expires_at, is_used)
+    VALUES (NULL, ?, ?, NOW(), NOW() + INTERVAL 10 MINUTE, 'false')
     ON DUPLICATE KEY UPDATE
     code = VALUES(code),
     created_at = NOW(),
     expires_at = NOW() + INTERVAL 10 MINUTE,
-    is_used = false')) {
+    is_used = 'false'")) {
         $stmt->bind_param('ss', $email, $otp);
         $stmt->execute();
 
