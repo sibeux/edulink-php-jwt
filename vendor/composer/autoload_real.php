9f878428f53fa80bd4875d22e1dc2cda633743ac
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-05-28 15:53:08 +0700

    Add PSR HTTP Message Interfaces and Dependencies
    - Introduced StreamInterface for handling data streams.
    - Added UploadedFileInterface for managing uploaded files.
    - Implemented UriInterface for URI representation and manipulation.
    - Included getallheaders polyfill for compatibility with PHP versions < 5.6.
    - Added Symfony Deprecation Contracts for triggering deprecation notices.
    - Included necessary licenses and README files for new packages.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit937f5de424acd34e58cf7639cb52ad9d
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit937f5de424acd34e58cf7639cb52ad9d::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
